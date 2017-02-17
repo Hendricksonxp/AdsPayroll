f9918bd4fe05340f33be5e9a1abc6715e7130b18
Chet Hendrickson <chet@hendricksonxp.com>
2017-02-16 21:40:28 -0500

    blah

--- AdsPayroll/Payroll.cs
+++ AdsPayroll/Payroll.cs
@@ -1,8 +1,12 @@
-﻿namespace AdsPayroll
+﻿using System.Collections.Generic;
+
+namespace AdsPayroll
 {
     public class Payroll
     {
         Employees employees = new Employees();
+        private string payDate;
+
         public void AddEmployee(Employee employee)
         {
             employees.Add(employee);
@@ -10,7 +14,11 @@
 
         public void Pay(string payDate)
         {
-
+            {
+                this.payDate = payDate;
+                employees.Pay(this);
+            }
+        
         }
 
         public int PayCount()
@@ -21,6 +29,32 @@
         public Employee GetEmployee(string id)
         {
             return employees.GetEmployee(id);
+        }
+
+        public void PayEmployee(Employee employee)
+        {
+            Dollars basePay = Dollars.Zero();
+            var grossPay = Dollars.Zero();
+            var federalTax = Dollars.Zero();
+            var netPay = Dollars.Zero();
+
+            basePay += employee.GetPayRate();
+            grossPay += basePay;
+            federalTax = grossPay*FederalTaxRate();
+            netPay = grossPay - federalTax;
+
+            var payment = new Payment(payDate, employee.GetId(), employee.GetName());
+            payment.SetBasePay(basePay);
+            payment.SetGrossPay(grossPay);
+            payment.SetFederalTax(federalTax);
+            payment.SetNetPay(netPay);
+            employee.AddPayment(payment);
+
+        }
+
+        private decimal FederalTaxRate()
+        {
+            return 0.25m;
         }
     }
 }
